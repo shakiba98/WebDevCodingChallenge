20edfbd613b93441afa80b9bd77c08edbdf72e95
Shakiba Iqbal <shakiba.iqbal@tspirits.com>
2021-01-30 20:44:59 +0500

    Artist model, APICallService implemented.

---
+++ artist-arena/src/app/models/artist.model.ts
@@ -0,0 +1,11 @@
+export class Artist {
+     _id: number;
+     name:	string;
+     url:	string;
+     image_url:	string;
+     thumb_url:	string;
+     facebook_page_url: string;
+     mbid: string;
+     tracker_count: number;
+     upcoming_event_count: number;
+  }
---
+++ artist-arena/src/app/services/apicall.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient,HttpParams } from '@angular/common/http';
+import { Artist } from '../models/artist.model';
+import { from, Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApicallService {
+
+  constructor(
+
+    private httpClient: HttpClient
+
+  ){}
+
+
+  getArtists() {
+    const params = new HttpParams({
+      fromObject: {
+        app_id: 'abc'
+      }
+    });
+   return this.httpClient.get(`https://rest.bandsintown.com/artists/abc`, {params: params}).
+       pipe(
+          map((data: Artist[]) => {
+            return data;
+          }), catchError( error => {
+            return throwError( 'Something went wrong!' );
+          })
+       )
+   }
+
+}
