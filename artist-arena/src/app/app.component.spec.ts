9ce0614133278d2f3086a5740e9a7be17278b2e3
Shakiba Iqbal <shakiba.iqbal@tspirits.com>
2021-02-01 22:40:10 +0500

    UI changes

--- artist-arena/src/app/app.component.spec.ts
+++ artist-arena/src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-
+import { ArtistSearchComponent } from './artist-search/artist-search.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        ArtistSearchComponent
       ],
     }).compileComponents();
   }));
